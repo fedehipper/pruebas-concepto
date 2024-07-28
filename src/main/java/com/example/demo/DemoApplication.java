2c351eb588a5b0ba680fa80abc67b38d17e476b8
hipper <federicohipper@gmail.com>
2024-07-28 17:15:08 -0300

    Commit inicial - formulario aysa

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import com.example.demo.controller.rest.FormularioPdf;
+import java.io.IOException;
+
+
+public class DemoApplication {
+
+    public static void main(String[] args) throws IOException {
+        //FormularioPdf.agregarFormulario();
+        FormularioPdf.agregarValoresAlFormulario();
+    }
+
+}
